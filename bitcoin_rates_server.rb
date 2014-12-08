f9dbe8bc39e2e6ed610c01ab58cd134d66a62650
Ryan Oaks <roaks3@gmail.com>
2014-12-07 19:07:48 -0800

    Hooked up firebase client and modified endpoint to return result of open-data/cryptocurrencies

--- bitcoin_rates_server.rb
+++ bitcoin_rates_server.rb
@@ -2,8 +2,14 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'sinatra'
+require 'firebase'
 
 get '/rates/bitcoin' do
-  'Test'
+  base_uri = 'https://publicdata-cryptocurrency.firebaseio.com'
+
+  firebase = Firebase::Client.new(base_uri)
+  response = firebase.get('bitcoin')
+
+  response.raw_body
 end
 
